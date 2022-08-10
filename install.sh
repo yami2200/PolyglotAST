318c6a377a27d0880ea79ff2a570fe6c37a4c12d
yami2200 <theyami22@gmail.com>
2022-08-10 13:34:50 +0200

    - Add install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Script working if the directories match the directories from the docker container
+# If you are not using Docker, make sure to run the file after followed the dockerfile steps from https://github.com/yami2200/polyglot-language-server
+
+echo "Copying Tree sitter files into Jsitter ..."
+cp ../tree-sitter/* ../jsitter/native/tree-sitter/
+echo "Copied successfully"
+
