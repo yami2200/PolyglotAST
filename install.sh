a6e08606f4d5eef3c1b9724784089ba1c2fbafd4
yami2200 <theyami22@gmail.com>
2022-08-10 13:39:38 +0200

    - update install.sh (recursive copy)

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 # If you are not using Docker, make sure to run the file after followed the dockerfile steps from https://github.com/yami2200/polyglot-language-server
 
 echo "Copying Tree sitter files into Jsitter ..."
-cp ../tree-sitter/* ../jsitter/native/tree-sitter/
+cp -r ../tree-sitter/* ../jsitter/native/tree-sitter/
 echo "Copied successfully !"
 
