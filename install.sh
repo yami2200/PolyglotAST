a5561fdb6f68afe590f12209a64e7f8f211e4e7c
yami2200 <theyami22@gmail.com>
2022-08-10 14:01:13 +0200

    - update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ cp -r ../tree-sitter/* ../jsitter/native/tree-sitter/
 echo "Copied successfully !"
 
 echo "Edit CMakeLists of Jsitter ..."
-sed -i '22iadd_library(tsjavascript SHARED grammars/tree-sitter-javascript/src/parser.c  grammars/tree-sitter-javascript/src/scanner.c)\nadd_library(tspython SHARED grammars/tree-sitter-python/src/parser.c grammars/tree-sitter-python/src/scanner.cc)' ../jsitter/native/CMakeLists.txt
+sed -i '20iadd_library(tsjavascript SHARED grammars/tree-sitter-javascript/src/parser.c  grammars/tree-sitter-javascript/src/scanner.c)\nadd_library(tspython SHARED grammars/tree-sitter-python/src/parser.c grammars/tree-sitter-python/src/scanner.cc)\ntarget_link_libraries(jsitter tsgo tsjava tsjavascript tspython)\n#[[' ../jsitter/native/CMakeLists.txt
+echo ']]' >> ../jsitter/native/CMakeLists.txt
 echo "Edited successfully !"
 
+echo "Copying Tree sitter Python and javascript into jsitter/native/grammars ..."
+cp -r ../tree-sitter-python ../jsitter/native/grammars/
+cp -r ../tree-sitter-javascript ../jsitter/native/grammars/
+echo "Copied successfully !"
